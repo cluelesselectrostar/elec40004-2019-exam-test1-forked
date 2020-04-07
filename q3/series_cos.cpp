57673a9fd88fd105fd5595eb52c4f26f5438e6bb
hubertsumarno <hubertjosumarno@gmail.com>
2020-04-07 08:17:17 +0100

    finished_T2.1&T2.2

--- q3/series_cos.cpp
+++ q3/series_cos.cpp
@@ -1,9 +1,34 @@
 #include "series_operations.hpp"
 
+float factorial(int n){
+    if (n==0)
+        return 1;
+    else
+        return 2*n*factorial(n-1);
+}
+
+class SeriesCos
+	:public Series
+{
+public:
+	SeriesCos()
+	{
+	}
+
+	virtual double a(int n) override
+	{
+		if(n%2==1){
+			return 0;
+		}else{
+			return pow(-1, n)/factorial(2*n);
+		}
+	}
+}
+
 
 Series *series_cos(Series *s)
 {
     assert(s->is_variable());
     assert(0);
+    return new SeriesCos(s);
 }
-
--- q3/series_sin.cpp
+++ q3/series_sin.cpp
@@ -1,7 +1,32 @@
 #include "series_operations.hpp"
 
+float factorial(int n){
+    if (n==0)
+        return 1;
+    else
+        return 2*n*factorial(n-1);
+}
+
+class SeriesSin
+	:public Series
+{
+public:
+	SeriesSin()
+	{
+	}
+
+	virtual double a(int n) override
+	{
+		if(n%2==0){
+			return 0;
+		}else{
+			return pow(-1, n)/factorial(2*n+1);
+	}
+}
+
 Series *series_sin(Series *s)
 {
     assert(s->is_variable());
     assert(0);
+    return new SeriesSin()
 }
